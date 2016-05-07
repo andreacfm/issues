434ddda21180691c0a6a9733b0e872bc2a42efa7
Andrea Campolonghi <acampolonghi@gmail.com>
2016-05-07 19:31:51 +0200

    Write and test the OptionParser

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -1,8 +1,19 @@
 defmodule IssuesTest do
   use ExUnit.Case
-  doctest Issues
+  import Issues.CLI, only: [ parse_args: 1 ]
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test ":help returned by options parsing with -h and --help" do
+    assert parse_args([]) == :help
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
   end
+
+  test "3 values are returned if they are given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+  end
+
+  test "default count if only 2 options are given" do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+
 end
