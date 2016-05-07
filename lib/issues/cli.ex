9c5c72b9ead858ceb723c9a4f4191148c21b8068
Andrea Campolonghi <acampolonghi@gmail.com>
2016-05-07 19:37:01 +0200

    Process :help

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,7 +6,9 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   def parse_args(argv) do
@@ -24,6 +26,13 @@ defmodule Issues.CLI do
         -> { user, project, @default_count }
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+      usage: issues <user> <project> [count | #{@default_count}]
+    """
+    System.halt(0)
   end
 
 end
